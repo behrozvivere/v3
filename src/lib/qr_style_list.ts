89b2b47332dc5a742edb60e5ec3bf95140e59255
Behroz <167723682+behrozvivere@users.noreply.github.com>
2024-12-16 19:19:34 +0000

    */qr_style_list.ts

--- src/lib/qr_style_list.ts
+++ src/lib/qr_style_list.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { QrStyleItemProps } from "@/lib/types";
+/*import { QrStyleItemProps } from "@/lib/types";*/
 
 export interface QrStyleItemProps {
   id: string;
